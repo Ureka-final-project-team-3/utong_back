9a33833b23207dfe79494f7f45da1eafcdbcc5b4
suhyun <suhyun9764@naver.com>
2025-07-05 16:57:09 +0900

    first commit

---
+++ src/main/java/com/ureka/team3/utong_backend/UtongBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ureka.team3.utong_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtongBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UtongBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ureka/team3/utong_backend/UtongBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ureka.team3.utong_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtongBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
