19d221765232dad3be34d75669f112fbe4bfbfe2
DoyeonLee-Luna <ldon77@naver.com>
2025-07-10 11:05:02 +0900

    refactor: 어노테이션 수정 (#16)

--- src/main/java/com/ureka/team3/utong_backend/mypage/dto/PointChargeRequestDto.java
+++ src/main/java/com/ureka/team3/utong_backend/mypage/dto/PointChargeRequestDto.java
@@ -1,9 +1,13 @@
 package com.ureka.team3.utong_backend.mypage.dto;
 
-import lombok.Getter;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 // 포인트 충전 입력
-@Getter
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PointChargeRequestDto {
     private Long chargedAmount; // 유저가 입력한 충전 금액
 }
