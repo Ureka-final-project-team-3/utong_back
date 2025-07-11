11f9f2bf17916af05e27d4cc85e6a08b7282ec9e
Jang Seung Beom <wkdtmf357@gmail.com>
2025-07-11 11:47:02 +0900

    feat: 수수료, 하한가, 하한 비율 조회 기능 Controller Layer

---
+++ src/main/java/com/ureka/team3/utong_backend/price/controller/PriceController.java
@@ -0,0 +1,26 @@
+package com.ureka.team3.utong_backend.price.controller;
+
+import com.ureka.team3.utong_backend.common.dto.ApiResponse;
+import com.ureka.team3.utong_backend.price.dto.PriceDto;
+import com.ureka.team3.utong_backend.price.service.PriceService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class PriceController {
+
+    private final PriceService priceService;
+    
+    @GetMapping("/prices")
+    public ResponseEntity<ApiResponse<PriceDto>> getPrice(
+            @RequestParam(defaultValue = "903ee67c-71b3-432e-bbd1-aaf5d5043376") String id
+    ) {
+        return ResponseEntity.ok(priceService.getPrice(id));
+    }
+}
