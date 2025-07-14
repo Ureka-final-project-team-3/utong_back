c24cb42561f35db4ec7b6cb6649989d7954dccc3
Jang Seung Beom <wkdtmf357@gmail.com>
2025-07-14 20:08:45 +0900

    feat: 기프티콘 교환 기능 Controller Layer

--- src/main/java/com/ureka/team3/utong_backend/gift/controller/GifticonController.java
+++ src/main/java/com/ureka/team3/utong_backend/gift/controller/GifticonController.java
@@ -1,15 +1,14 @@
 package com.ureka.team3.utong_backend.gift.controller;
 
+import com.ureka.team3.utong_backend.auth.entity.Account;
 import com.ureka.team3.utong_backend.common.dto.ApiResponse;
 import com.ureka.team3.utong_backend.gift.dto.GifticonResponseDto;
 import com.ureka.team3.utong_backend.gift.service.GifticonService;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -33,5 +32,13 @@ public class GifticonController {
     @GetMapping("/gifticons/count")
     public ResponseEntity<ApiResponse<Long>> getGifticonCount() {
         return ResponseEntity.ok(gifticonService.getGifticonCount());
+    }
+
+    @PostMapping("/gifticons/{gifticonId}/exchange")
+    public ResponseEntity<ApiResponse<Void>> exchangeGifticon(
+            @PathVariable("gifticonId") String gifticonId,
+            @AuthenticationPrincipal Account account
+    ) {
+        return ResponseEntity.ok(gifticonService.exchangeGifticon(gifticonId, account.getId()));
     }
 }
