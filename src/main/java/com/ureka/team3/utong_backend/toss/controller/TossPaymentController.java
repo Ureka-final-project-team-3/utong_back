2bf71a4b61f256c28da4f9ea769558025d27c45a
DoyeonLee-Luna <ldon77@naver.com>
2025-07-15 11:11:05 +0900

    feat: 포인트 충전 토스 연동 결제 승인 (#50)

---
+++ src/main/java/com/ureka/team3/utong_backend/toss/controller/TossPaymentController.java
@@ -0,0 +1,31 @@
+package com.ureka.team3.utong_backend.toss.controller;
+
+import com.ureka.team3.utong_backend.auth.entity.Account;
+import com.ureka.team3.utong_backend.common.dto.ApiResponse;
+import com.ureka.team3.utong_backend.mypage.dto.PointChargeResponseDto;
+import com.ureka.team3.utong_backend.toss.dto.TossPaymentConfirmRequestDto;
+import com.ureka.team3.utong_backend.toss.service.TossPaymentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/payments/toss")
+@RequiredArgsConstructor
+public class TossPaymentController {
+
+    private final TossPaymentService tossPaymentService;
+
+    @PostMapping("/confirm")
+    public ResponseEntity<ApiResponse<PointChargeResponseDto>> confirmPayment(
+            @AuthenticationPrincipal Account account,
+            @RequestBody TossPaymentConfirmRequestDto requestDto) {
+
+        PointChargeResponseDto response = tossPaymentService.confirmAndCharge(account, requestDto);
+        return ResponseEntity.ok(ApiResponse.success(response));
+    }
+}
---
+++ src/main/java/com/ureka/team3/utong_backend/toss/dto/TossPaymentConfirmRequestDto.java
@@ -0,0 +1,12 @@
+package com.ureka.team3.utong_backend.toss.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TossPaymentConfirmRequestDto {
+    private String paymentKey;
+    private String orderId;
+    private Long amount;
+}
