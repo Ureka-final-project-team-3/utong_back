212ffa140758b5bd61583196a7da2695c2de1399
DoyeonLee-Luna <ldon77@naver.com>
2025-07-17 02:34:55 +0900

    feat: 본인 구매 대기 내역 조회 쿼리 추가 (#63)

--- src/main/java/com/ureka/team3/utong_backend/datatrade/repository/BuyDataRequestRepository.java
+++ src/main/java/com/ureka/team3/utong_backend/datatrade/repository/BuyDataRequestRepository.java
@@ -4,11 +4,27 @@ import com.ureka.team3.utong_backend.datatrade.entity.BuyDataRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 public interface BuyDataRequestRepository extends JpaRepository<BuyDataRequest, String> {
     @Query("SELECT COUNT(bdr) > 0 FROM BuyDataRequest bdr WHERE bdr.lineId = :lineId AND (bdr.status = '002' or bdr.status='003')")
     boolean existsWaitingRequestByLineId(@Param("lineId") String id);
+
+    // 본인 구매 대기 내역 조회
+    @Query("""
+        SELECT b FROM BuyDataRequest b
+        WHERE b.account.id = :accountId
+        AND NOT EXISTS (
+            SELECT c FROM Contract c WHERE c.buyDataRequest.id = b.id
+        )
+        AND b.createdAt >= :fromDate
+    """)
+    List<BuyDataRequest> findWaitingPurchasesByAccountId(
+            @Param("accountId") String accountId,
+            @Param("fromDate") LocalDateTime fromDate
+    );
 }
