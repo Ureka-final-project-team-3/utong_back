2f197b522ad0c2a30662a5a245e62565cbb41537
DoyeonLee-Luna <ldon77@naver.com>
2025-07-17 02:35:26 +0900

    feat: 본인 구매 완료, 판매 완료 조회 기능 (#63)

--- src/main/java/com/ureka/team3/utong_backend/datatrade/repository/ContractRepository.java
+++ src/main/java/com/ureka/team3/utong_backend/datatrade/repository/ContractRepository.java
@@ -2,8 +2,35 @@ package com.ureka.team3.utong_backend.datatrade.repository;
 
 import com.ureka.team3.utong_backend.datatrade.entity.Contract;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 public interface ContractRepository extends JpaRepository<Contract, String> {
+
+    // 구매 완료
+    @Query("""
+        SELECT c FROM Contract c
+        WHERE c.buyDataRequest.account.id = :accountId
+        AND c.createdAt >= :fromDate
+    """)
+    List<Contract> findCompletedPurchasesByAccountId(
+            @Param("accountId") String accountId,
+            @Param("fromDate") LocalDateTime fromDate
+    );
+
+    // 판매 완료
+    @Query("""
+        SELECT c FROM Contract c
+        WHERE c.saleDataRequest.account.id = :accountId
+        AND c.createdAt >= :fromDate
+    """)
+    List<Contract> findCompletedSalesByAccountId(
+            @Param("accountId") String accountId,
+            @Param("fromDate") LocalDateTime fromDate
+    );
 }
