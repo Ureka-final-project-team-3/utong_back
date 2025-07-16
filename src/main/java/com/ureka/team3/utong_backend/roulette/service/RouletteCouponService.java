52958ba32cd5e3f8cff79038969a3ef72f8e6ee2
son <cliff615@naver.com>
2025-07-16 17:00:55 +0900

    fix : 룰렛 쿠폰 및 기프티콘으로 발급(#66)

--- src/main/java/com/ureka/team3/utong_backend/roulette/service/RouletteCouponService.java
+++ src/main/java/com/ureka/team3/utong_backend/roulette/service/RouletteCouponService.java
@@ -1,21 +1,25 @@
 package com.ureka.team3.utong_backend.roulette.service;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.ureka.team3.utong_backend.auth.entity.Account;
-import com.ureka.team3.utong_backend.user.entity.User;
-import com.ureka.team3.utong_backend.user.repository.UserRepository;
 import com.ureka.team3.utong_backend.common.dto.ApiResponse;
 import com.ureka.team3.utong_backend.common.exception.ErrorCode;
 import com.ureka.team3.utong_backend.coupon.entity.Coupon;
 import com.ureka.team3.utong_backend.coupon.entity.UserCoupon;
 import com.ureka.team3.utong_backend.coupon.repository.MyCouponRepository;
+import com.ureka.team3.utong_backend.gift.entity.UserGifticon;
+import com.ureka.team3.utong_backend.gift.repository.MyGifticonRepository;
 import com.ureka.team3.utong_backend.roulette.entity.RouletteEvent;
+import com.ureka.team3.utong_backend.user.entity.User;
+import com.ureka.team3.utong_backend.user.repository.UserRepository;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.time.LocalDateTime;
-import java.util.UUID;
 
 @Slf4j
 @Service
@@ -23,7 +27,7 @@ import java.util.UUID;
 public class RouletteCouponService {
     private final MyCouponRepository myCouponRepository;
     private final UserRepository userRepository;
-    
+    private final MyGifticonRepository myGifticonRepository;
     @Transactional
     public ApiResponse<Void> issueWinnerCoupon(Account account, RouletteEvent event) {
         try {
@@ -33,16 +37,34 @@ public class RouletteCouponService {
                     .orElse(null);
             if (user == null) return ApiResponse.fail(ErrorCode.USER_NOT_FOUND);
             Coupon rewardCoupon = event.getRewardCoupon();
-            UserCoupon userCoupon = UserCoupon.builder()
-                    .id(UUID.randomUUID().toString())
-                    .user(user)
-                    .coupon(rewardCoupon)
-                    .createdAt(LocalDateTime.now())
-                    .expiredAt(LocalDateTime.now().plusDays(3).withHour(23).withMinute(59).withSecond(59))
-                    .build();
+            if ("002".equals(rewardCoupon.getCouponCode())) 
+            {
+            	UserGifticon userGifticon = UserGifticon.builder()
+//            							.id(UUID.randomUUID().toString())
+            							.user(user)
+            							.gifticon(rewardCoupon.getGifticon())
+            							.isActive(true)
+            							.createdAt(LocalDateTime.now())
+            	                        .expiredAt(LocalDateTime.now().plusDays(3).withHour(23).withMinute(59).withSecond(59))
+            							.build();
+            	
+            	myGifticonRepository.save(userGifticon);
+                return ApiResponse.success("기프티콘이 성공적으로 발급되었습니다", null);
+            }
+            else
+            {
+            	UserCoupon userCoupon = UserCoupon.builder()
+                        .id(UUID.randomUUID().toString())
+                        .user(user)
+                        .coupon(rewardCoupon)
+                        .createdAt(LocalDateTime.now())
+                        .expiredAt(LocalDateTime.now().plusDays(3).withHour(23).withMinute(59).withSecond(59))
+                        .build();
+                
+                myCouponRepository.save(userCoupon);
+                return ApiResponse.success("쿠폰이 성공적으로 발급되었습니다", null);
+            }
             
-            myCouponRepository.save(userCoupon);
-            return ApiResponse.success("쿠폰이 성공적으로 발급되었습니다", null);
             
         } catch (Exception e) {
             return ApiResponse.fail(ErrorCode.INTERNAL_SERVER_ERROR);
