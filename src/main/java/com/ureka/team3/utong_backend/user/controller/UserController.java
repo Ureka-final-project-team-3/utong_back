0c51bcad7969d70d4e0572b4ba1760de06989161
suhyun <suhyun9764@naver.com>
2025-07-16 11:24:04 +0900

    build : test

--- src/main/java/com/ureka/team3/utong_backend/user/controller/UserController.java
+++ src/main/java/com/ureka/team3/utong_backend/user/controller/UserController.java
@@ -5,12 +5,14 @@ import com.ureka.team3.utong_backend.common.dto.ApiResponse;
 import com.ureka.team3.utong_backend.user.dto.MyInfoResponseDto;
 import com.ureka.team3.utong_backend.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/mypage")
 @RequiredArgsConstructor
@@ -21,6 +23,7 @@ public class UserController {
     // 내 정보 조회(이름, 이메일, 포인트, 핸드폰, 남은 데이터 용량)
     @GetMapping("/info")
     public ResponseEntity<ApiResponse<MyInfoResponseDto>> getMyInfo(@AuthenticationPrincipal Account account){
+        log.info(account.getId());
         return ResponseEntity.ok(ApiResponse.success(userService.getMyInfo(account)));
     }
 }
