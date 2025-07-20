b29e0dd6c1eaf1e656fe37e9dae5f4073f3c2e91
DoyeonLee-Luna <ldon77@naver.com>
2025-07-20 11:44:22 +0900

    refactor: payment controller swagger 설명 추가 (#94)

--- src/main/java/com/ureka/team3/utong_backend/payment/controller/PaymentController.java
+++ src/main/java/com/ureka/team3/utong_backend/payment/controller/PaymentController.java
@@ -5,6 +5,7 @@ import com.ureka.team3.utong_backend.common.dto.ApiResponse;
 import com.ureka.team3.utong_backend.point.dto.PointChargeResponseDto;
 import com.ureka.team3.utong_backend.payment.dto.PaymentConfirmRequestDto;
 import com.ureka.team3.utong_backend.payment.service.PaymentService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -33,6 +34,7 @@ public class PaymentController {
 
     // 결제 승인
     @PostMapping("/confirm")
+    @Operation(summary = "포인트 충전 결제 연동", description = "포인트 충전 시 결제 api 연동으로 결제 가능합니다. 수수료 면제 쿠폰이 있는 경우 적용할 수 있습니다.")
     public ResponseEntity<ApiResponse<PointChargeResponseDto>> confirmPayment(@AuthenticationPrincipal Account account, @RequestBody PaymentConfirmRequestDto requestDto) {
         return ResponseEntity.ok(ApiResponse.success(paymentService.confirmAndCharge(account, requestDto)));
     }
