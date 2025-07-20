347d852697ee0ab4463c8d43296ec39b6c24057c
suhyun <suhyun9764@naver.com>
2025-07-21 04:07:46 +0900

    refactor : 컨트롤러 분리

--- src/main/java/com/ureka/team3/utong_backend/datatrade/controller/DataTradeController.java
+++ src/main/java/com/ureka/team3/utong_backend/datatrade/controller/DataTradeController.java
@@ -4,15 +4,13 @@ import com.ureka.team3.utong_backend.auth.entity.Account;
 import com.ureka.team3.utong_backend.datatrade.dto.DataTradeDto;
 import com.ureka.team3.utong_backend.datatrade.dto.TradeHistoryRequestDto;
 import com.ureka.team3.utong_backend.datatrade.facade.DataTradeFacade;
-import com.ureka.team3.utong_backend.datatrade.service.chart.SseService;
+import com.ureka.team3.utong_backend.datatrade.handler.SseHandler;
 import com.ureka.team3.utong_backend.datatrade.service.query.TradeQueryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @Slf4j
 @RestController
@@ -22,7 +20,6 @@ public class DataTradeController {
 
     private final DataTradeFacade dataTradeFacade;
     private final TradeQueryService tradeQueryService;
-    private final SseService sseService;
 
     @PostMapping("/purchase")
     public ResponseEntity getPurchaseHistory(@AuthenticationPrincipal Account account, @RequestBody DataTradeDto.BuyDataRequestDto buyRequestDto) {
@@ -45,10 +42,5 @@ public class DataTradeController {
     public ResponseEntity requestSale(@AuthenticationPrincipal Account account, @RequestBody DataTradeDto.SaleDataRequestDto saleRequestDto) {
         log.info("로그인한 id : {}", account.getId());
         return ResponseEntity.ok(dataTradeFacade.requestSale(account, saleRequestDto));
-    }
-
-    @GetMapping(value = "/current-prices/stream/{dataCode}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public SseEmitter streamCurrentPrices(@PathVariable("dataCode") String dataCode) {
-        return sseService.connectForCurrentPrice(dataCode);
     }
 }
