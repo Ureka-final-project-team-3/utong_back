1fac7f341de1b31f1be2a762546da2cd4eae5d61
son <cliff615@naver.com>
2025-07-21 21:23:11 +0900

    fix: accesstoken 만료시간 1분 (#72)

--- src/main/java/com/ureka/team3/utong_backend/auth/util/JwtProperties.java
+++ src/main/java/com/ureka/team3/utong_backend/auth/util/JwtProperties.java
@@ -10,6 +10,6 @@ import lombok.Data;
 public class JwtProperties {
     
     private String secret = "mySecretKey1234567890123456789012345678901234567890";
-    private Long accessTokenExpiration = 3600000L; // 1시간
+    private Long accessTokenExpiration = 60000L; // 1시간
     private Long refreshTokenExpiration = 604800000L; // 7일
 }
