5a2b6bb68a6f77bdc136977ded2d67c9fe96f47c
DoyeonLee-Luna <ldon77@naver.com>
2025-07-22 17:11:02 +0900

    feat: 본인 쿠폰 사용 pathvariable -> requestbody 로 변경으로 인한 dto 생성 (#123)

---
+++ src/main/java/com/ureka/team3/utong_backend/coupon/dto/CouponUseRequestDto.java
@@ -0,0 +1,13 @@
+package com.ureka.team3.utong_backend.coupon.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CouponUseRequestDto {
+    private String userCouponId;
+}
+
