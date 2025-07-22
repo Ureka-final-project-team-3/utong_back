070a9721ccc21b96ce572aab719cebcfbbe2b2fe
son <cliff615@naver.com>
2025-07-22 11:40:43 +0900

    fix : cors 추가

--- src/main/java/com/ureka/team3/utong_backend/config/CorsConfig.java
+++ src/main/java/com/ureka/team3/utong_backend/config/CorsConfig.java
@@ -23,6 +23,7 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         List<String> origins = List.of(allowedOrigins.split(","));
+        origins.add("http://d2rykyv7hqvhku.cloudfront.net");
         config.setAllowedOriginPatterns(origins);
 
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
