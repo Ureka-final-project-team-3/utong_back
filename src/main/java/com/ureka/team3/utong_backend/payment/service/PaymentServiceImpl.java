7251f9662c015fc460f2eae1b630e848d237ef28
DoyeonLee-Luna <ldon77@naver.com>
2025-07-24 00:48:18 +0900

    refactor: 포인트 충전 결제 연동 리팩토링 (#140)

--- src/main/java/com/ureka/team3/utong_backend/payment/service/PaymentServiceImpl.java
+++ src/main/java/com/ureka/team3/utong_backend/payment/service/PaymentServiceImpl.java
@@ -9,12 +9,13 @@ import com.ureka.team3.utong_backend.common.exception.business.NotFeeWaiveCoupon
 import com.ureka.team3.utong_backend.coupon.entity.UserCoupon;
 import com.ureka.team3.utong_backend.coupon.repository.MyCouponRepository;
 import com.ureka.team3.utong_backend.datatrade.utils.TradeCalculator;
+import com.ureka.team3.utong_backend.payment.dto.PaymentApproveResponse;
+import com.ureka.team3.utong_backend.payment.dto.PaymentConfirmRequestDto;
 import com.ureka.team3.utong_backend.point.dto.PointChargeResponseDto;
 import com.ureka.team3.utong_backend.point.entity.PointChargeHistory;
 import com.ureka.team3.utong_backend.point.repository.PointChargeHistoryRepository;
-import com.ureka.team3.utong_backend.payment.dto.PaymentApproveResponse;
-import com.ureka.team3.utong_backend.payment.dto.PaymentConfirmRequestDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -30,6 +31,7 @@ import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class PaymentServiceImpl implements PaymentService {
 
     private final RestTemplate restTemplate = new RestTemplate();
@@ -38,39 +40,149 @@ public class PaymentServiceImpl implements PaymentService {
     private final MyCouponRepository myCouponRepository;
     private final TradeCalculator tradeCalculator;
 
-//    private static final double FEE_RATE = 0.025;
-
-
     @Value("${toss.secret-key}")
     private String tossSecretKey;
 
-//    // 토스 결제 시작
-//    @Override
-//    public TossPaymentResponseDto startPayment(Account account, TossPaymentRequestDto requestDto) {
-//        // 1. orderId, 금액, 사용자 정보 등을 저장하거나 검증
-//        String orderId = requestDto.getOrderId();
-//        Long amount = requestDto.getAmount();
+//    @Override   // todo : 메서드 리팩터링 : 책임과 역할에 맞게 메서드 분리 ( 클래스도 분리할거 있는지 고민해보길)
+//    public PointChargeResponseDto confirmAndCharge(Account account, PaymentConfirmRequestDto requestDto) {
+//        System.out.println("[결제 승인 요청] paymentKey: " + requestDto.getPaymentKey());
+//        System.out.println("[결제 승인 요청] orderId: " + requestDto.getOrderId());
+//        System.out.println("[결제 승인 요청] amount: " + requestDto.getAmount());
+//        System.out.println("[결제 승인 요청] couponId: " + requestDto.getUserCouponId());
+//        // todo : 로그 수정
 //
-//        // 2. 결제창 URL 생성
-//        // 실제로는 프론트에서 Checkout SDK가 처리, 백에서는 보통 orderId만 관리
-//        String fakeCheckoutUrl = "https://pay.toss.im/checkout/" + orderId;
+//        // 1. 토스 결제 승인 요청
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.setBasicAuth(tossSecretKey, "");
+//        headers.setContentType(MediaType.APPLICATION_JSON);
 //
-//        return TossPaymentResponseDto.builder()
-//                .paymentKey(null) // 결제 후 승인 시에야 paymentKey가 생김
-//                .checkoutUrl(fakeCheckoutUrl)
+//        Map<String, Object> body = new HashMap<>();
+//        body.put("paymentKey", requestDto.getPaymentKey());
+//        body.put("orderId", requestDto.getOrderId());
+//        body.put("amount", requestDto.getAmount());
+//
+//        HttpEntity<Map<String, Object>> httpRequest = new HttpEntity<>(body, headers);
+//
+//        ResponseEntity<PaymentApproveResponse> response = restTemplate.postForEntity(
+//                "https://api.tosspayments.com/v1/payments/confirm", // todo : 환경 변수로 (도연이꺼 아님)
+//                httpRequest,
+//                PaymentApproveResponse.class
+//        );
+//
+//
+//        // Todo : log 수정
+//        System.out.println(" Toss 응답 원문: " + response);
+//        System.out.println(" Toss 응답 body: " + response.getBody());
+//        System.out.println(" Toss amount: " + response.getBody().getTotalAmount());
+//
+//        if (!response.getStatusCode().is2xxSuccessful() || response.getBody() == null) {
+//            throw new RuntimeException("토스 결제 승인 실패");
+//        }
+//
+//        PaymentApproveResponse approve = response.getBody();
+//        Long charged = approve.getTotalAmount();
+//
+//        // 2. 쿠폰 처리
+//        UserCoupon usedCoupon = null;
+//        boolean isFeeWaived = false;
+//
+//        if (requestDto.getUserCouponId() != null) {
+//            usedCoupon = myCouponRepository.findById(requestDto.getUserCouponId())
+//                    .orElseThrow(CouponNotFoundException::new);
+//
+//            if (!"002".equals(usedCoupon.getStatus())) {
+//                throw new InvalidCouponStatusException();
+//            }
+//
+//            if (!"001".equals(usedCoupon.getCoupon().getCouponCode())) {
+//                throw new NotFeeWaiveCouponException();
+//            }
+//
+//            if (usedCoupon.getExpiredAt() != null &&
+//                    usedCoupon.getExpiredAt().isBefore(LocalDateTime.now())) {
+//                throw new CouponExpiredException();
+//            }
+//
+//            isFeeWaived = true;
+//        }
+//
+//        // 3. 수수료 계싼
+////        Long fee = isFeeWaived ? 0L : Math.round(charged * FEE_RATE);
+//        Long fee = isFeeWaived ? 0L : tradeCalculator.calculateSubtractFee(charged);
+//        Long finalAmount = charged - fee;
+//
+//        // 4. 포인트 적립
+//        account.addMileage(finalAmount);
+//        accountRepository.save(account);
+//
+//        PointChargeHistory history = PointChargeHistory.builder()
+//                .id(UUID.randomUUID().toString())
+//                .account(account)
+//                .chargedAmount(charged)
+//                .feeAmount(fee)
+//                .finalAmount(finalAmount)
+//                .build();
+//
+//        pointChargeHistoryRepository.save(history);
+//
+//        // 5. 쿠폰 상태 변경
+//        if (usedCoupon != null) {
+//            usedCoupon.markAsUsed();  // 사용 완료
+//            myCouponRepository.save(usedCoupon);
+//        }
+//
+//        return PointChargeResponseDto.builder()
+//                .chargedAmount(charged)
+//                .feeAmount(fee)
+//                .finalAmount(finalAmount)
+//                .updatedMileage(account.getMileage())
 //                .build();
 //    }
-
-
     @Override   // todo : 메서드 리팩터링 : 책임과 역할에 맞게 메서드 분리 ( 클래스도 분리할거 있는지 고민해보길)
     public PointChargeResponseDto confirmAndCharge(Account account, PaymentConfirmRequestDto requestDto) {
-        System.out.println("[결제 승인 요청] paymentKey: " + requestDto.getPaymentKey());
-        System.out.println("[결제 승인 요청] orderId: " + requestDto.getOrderId());
-        System.out.println("[결제 승인 요청] amount: " + requestDto.getAmount());
-        System.out.println("[결제 승인 요청] couponId: " + requestDto.getUserCouponId());
-        // todo : 로그 수정
+        logPayRequest(requestDto);
 
         // 1. 토스 결제 승인 요청
+        PaymentApproveResponse approve = approvePayment(requestDto);
+        Long charged = approve.getTotalAmount();
+
+        // 2. 쿠폰 검증 및 처리
+        boolean isFeeWaived = false;
+        UserCoupon userCoupon = null;
+        if(requestDto.getUserCouponId() != null) {
+            userCoupon = validateCouponIfPresent(requestDto.getUserCouponId());
+            isFeeWaived = true;
+        }
+
+        // 3. 수수료 계산
+        Long fee = calculateFee(charged, isFeeWaived);
+        Long finalAmount = charged - fee;
+
+        // 4. 포인트 적립 및 내역 저장
+        savePointCharge(account, charged, fee, finalAmount);
+
+        // 5. 쿠폰 상태 업데이트
+        markCouponAsUsedIfExists(userCoupon);
+
+        return PointChargeResponseDto.builder()
+                .chargedAmount(charged)
+                .feeAmount(fee)
+                .finalAmount(finalAmount)
+                .updatedMileage(account.getMileage())
+                .build();
+    }
+
+    // 결제 승인 요청에 포함된 정보
+    private void logPayRequest(PaymentConfirmRequestDto requestDto) {
+        log.info("[결제 승인 요청] patmentKey: {}", requestDto.getPaymentKey());
+        log.info("[결제 승인 요청] paymentKey: {}", requestDto.getPaymentKey());
+        log.info("[결제 승인 요청] amount: {}", requestDto.getAmount());
+        log.info("[결제 승인 요청] couponId: {}", requestDto.getUserCouponId());
+        // todo : 로그 수정 - 완료
+    }
+
+    // 결제 승인 요청 -> 결제 결과 반환
+    private PaymentApproveResponse approvePayment(PaymentConfirmRequestDto requestDto) {
         HttpHeaders headers = new HttpHeaders();
         headers.setBasicAuth(tossSecretKey, "");
         headers.setContentType(MediaType.APPLICATION_JSON);
@@ -88,49 +200,39 @@ public class PaymentServiceImpl implements PaymentService {
                 PaymentApproveResponse.class
         );
 
-
-        // Todo : log 수정
-        System.out.println(" Toss 응답 원문: " + response);
-        System.out.println(" Toss 응답 body: " + response.getBody());
-        System.out.println(" Toss amount: " + response.getBody().getTotalAmount());
-
         if (!response.getStatusCode().is2xxSuccessful() || response.getBody() == null) {
-            throw new RuntimeException("토스 결제 승인 실패");
+            throw new RuntimeException("결제 승인 실패");
+        }
+        return response.getBody();
+    }
+
+    // 쿠폰 유효성 검증
+    private UserCoupon validateCouponIfPresent(String userCouponId) {
+        UserCoupon coupon = myCouponRepository.findById(userCouponId).orElseThrow(CouponNotFoundException::new);
+
+        if ( !"002".equals(coupon.getStatus()) ) {
+            throw new InvalidCouponStatusException();
         }
 
-        PaymentApproveResponse approve = response.getBody();
-        Long charged = approve.getTotalAmount();
-
-        // 2. 쿠폰 처리
-        UserCoupon usedCoupon = null;
-        boolean isFeeWaived = false;
-
-        if (requestDto.getUserCouponId() != null) {
-            usedCoupon = myCouponRepository.findById(requestDto.getUserCouponId())
-                    .orElseThrow(CouponNotFoundException::new);
-
-            if (!"002".equals(usedCoupon.getStatus())) {
-                throw new InvalidCouponStatusException();
-            }
-
-            if (!"001".equals(usedCoupon.getCoupon().getCouponCode())) {
-                throw new NotFeeWaiveCouponException();
-            }
-
-            if (usedCoupon.getExpiredAt() != null &&
-                    usedCoupon.getExpiredAt().isBefore(LocalDateTime.now())) {
-                throw new CouponExpiredException();
-            }
-
-            isFeeWaived = true;
+        if ( !"001".equals(coupon.getCoupon().getCouponCode())) {
+            throw new NotFeeWaiveCouponException();
         }
 
-        // 3. 수수료 계싼
-//        Long fee = isFeeWaived ? 0L : Math.round(charged * FEE_RATE);
-        Long fee = isFeeWaived ? 0L : tradeCalculator.calculateSubtractFee(charged);
-        Long finalAmount = charged - fee;
+        if ( coupon.getExpiredAt() != null && coupon.getExpiredAt().isBefore(LocalDateTime.now())) {
+            throw new CouponExpiredException();
+        }
 
-        // 4. 포인트 적립
+        return coupon;
+
+    }
+
+    // 수수료 계산
+    private Long calculateFee(Long charged, boolean isFeeWaived) {
+        return isFeeWaived ? 0L : tradeCalculator.calculateSubtractFee(charged);
+    }
+
+    // 포인트 충전 내역 저장
+    private void savePointCharge(Account account, Long charged, Long fee, Long finalAmount) {
         account.addMileage(finalAmount);
         accountRepository.save(account);
 
@@ -141,20 +243,16 @@ public class PaymentServiceImpl implements PaymentService {
                 .feeAmount(fee)
                 .finalAmount(finalAmount)
                 .build();
-
         pointChargeHistoryRepository.save(history);
-
-        // 5. 쿠폰 상태 변경
-        if (usedCoupon != null) {
-            usedCoupon.markAsUsed();  // 사용 완료
-            myCouponRepository.save(usedCoupon);
-        }
-
-        return PointChargeResponseDto.builder()
-                .chargedAmount(charged)
-                .feeAmount(fee)
-                .finalAmount(finalAmount)
-                .updatedMileage(account.getMileage())
-                .build();
     }
+
+    // 쿠폰 존재할 경우 사용 처리 저장
+    private void markCouponAsUsedIfExists(UserCoupon userCoupon) {
+        if ( userCoupon != null) {
+            userCoupon.markAsUsed();
+            myCouponRepository.save(userCoupon);
+        }
+    }
+
+
 }
